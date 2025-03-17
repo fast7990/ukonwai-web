7882271d870e32b409c4d340be68a73a2c1d6fc4
JayClock <35857909+JayClock@users.noreply.github.com>
2025-03-17 13:23:53 +0800

    feat: get doc list - basic

--- src/app/work/[id]/@directory/action.ts
+++ src/app/work/[id]/@directory/action.ts
@@ -1,12 +1,23 @@
 'use server'
 
 import { db } from '@/db/db'
+import { redirect } from 'next/navigation'
 
 export async function create() {
-  await db.doc.create({
+  const newDoc = await db.doc.create({
     data: {
-      title: '新建文档',
-      content: '文档内容',
+      title: `新建文档${Date.now().toString().slice(-4)}`,
+      content: '',
+    },
+  })
+  redirect(`/work/${newDoc.id}`)
+}
+
+export async function getDocList() {
+  return await db.doc.findMany({
+    select: {
+      id: true,
+      title: true,
     },
   })
 }
--- src/app/work/[id]/@directory/page.tsx
+++ src/app/work/[id]/@directory/page.tsx
@@ -1,21 +1,32 @@
 import { FileText } from 'lucide-react'
-import { create } from './action'
+import { create, getDocList } from './action'
 import CreateSubmitButton from './create-submit-button'
+import { cn } from '@/lib/utils'
 
-export default async function Directory(props: { params: Promise<{ id: string }> }) {
-  const { params } = await props
-  const { id } = await params
+export default async function Directory(props: { params: { id: string } }) {
+  const params = await props.params
+  const list = await getDocList()
   return (
-    <div className=" h-[800px]">
-      {[1, 2, 3, 4, 5, 6, 7].map((i) => (
-        <div
-          key={i}
-          className="inline-flex items-center w-full p-2 py-1 cursor-pointer hover:bg-card hover:text-secondary-foreground"
-        >
-          <FileText className="h-4 w-4" />
-          &nbsp;动物农场{i} {id}
-        </div>
-      ))}
+    <div>
+      {list.map((doc) => {
+        const { id, title } = doc
+        let isCurrent = false
+        if (id === parseInt(params.id, 10)) isCurrent = true
+
+        return (
+          <div
+            key={id}
+            className={cn(
+              'inline-flex items-center w-full p-2 py-1 cursor-pointer hover:text-secondary-foreground',
+              isCurrent ? 'bg-card' : 'hover:bg-card'
+            )}
+          >
+            <FileText className="h-4 w-4" />
+            &nbsp;{title}
+          </div>
+        )
+      })}
+
       <form action={create}>
         <CreateSubmitButton />
       </form>
