5e4897a1999b638e9e948d0ba33d6a3b69109865
JayClock <891853602@qq.com>
2025-03-18 13:12:18 +0800

    feat: nextauth adapter

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,14 @@
 import NextAuth from 'next-auth'
 import GitHub from 'next-auth/providers/github'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { db } from '@/db/db'
+import { Adapter } from '@auth/core/adapters'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   theme: {
     logo: 'https://next-auth.js.org/img/logo/logo-sm.png',
   },
+  adapter: PrismaAdapter(db) as Adapter,
   providers: [GitHub],
   basePath: '/auth',
   callbacks: {
