1e3af7deeb88c3c1f24d48ecc421a104e23e330e
JayClock <891853602@qq.com>
2025-03-19 15:13:39 +0800

    fix: editor - bubble menu in codeblock

--- src/components/editor/menus/text-menu/index.tsx
+++ src/components/editor/menus/text-menu/index.tsx
@@ -6,6 +6,7 @@ import AlignMenu from '@/components/editor/menus/text-menu/align-menu'
 import MoreMenu from '@/components/editor/menus/text-menu/more-menu'
 import ContentTypeMenu from '@/components/editor/menus/text-menu/content-type'
 import BasicMenu from '@/components/editor/menus/text-menu/basic-menu'
+import { isTextSelected } from '@/components/editor/utils/isTextSelected'
 
 interface IProps {
   editor: Editor | null
@@ -15,8 +16,21 @@ export default function TextMenu(props: IProps) {
   const { editor } = props
   if (editor == null) return
 
+  function shouldShow(editor: Editor) {
+    // 某些类型，不显示文本菜单
+    if (editor.isActive('codeBlock')) {
+      return false
+    }
+    return isTextSelected({ editor })
+  }
+
   return (
-    <BubbleMenu editor={editor} tippyOptions={{ duration: 100 }} updateDelay={100}>
+    <BubbleMenu
+      editor={editor}
+      tippyOptions={{ duration: 100 }}
+      updateDelay={100}
+      shouldShow={() => shouldShow(editor)}
+    >
       <div
         className="
           border rounded p-1 shadow
---
+++ src/components/editor/utils/isTextSelected.ts
@@ -0,0 +1,16 @@
+import { Editor, isTextSelection } from '@tiptap/react'
+
+export const isTextSelected = ({ editor }: { editor: Editor }) => {
+  const {
+    state: {
+      doc,
+      selection,
+      selection: { empty, from, to },
+    },
+  } = editor
+  const isEmptyTextBlock = !doc.textBetween(from, to).length && isTextSelection(selection)
+  if (empty || isEmptyTextBlock || !editor.isEditable) {
+    return false
+  }
+  return true
+}
